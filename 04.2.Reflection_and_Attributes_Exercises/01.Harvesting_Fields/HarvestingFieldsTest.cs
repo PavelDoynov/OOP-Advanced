71cae6472f9b942971c2020be3369dd1542aba67
Pavel Doynov <paveldoynov@Pavels-MacBook-Pro.local>
2018-04-06 20:12:26 +0300

    OOP Advanced with C# course

---
+++ 04.2.Reflection_and_Attributes_Exercises/01.Harvesting_Fields/HarvestingFieldsTest.cs
@@ -0,0 +1,63 @@
+﻿ namespace P01_HarvestingFields
+{
+    using System;
+    using System.Linq;
+    using System.Reflection;
+
+    public class HarvestingFieldsTest
+    {
+        public static void Main()
+        {
+            //TODO put your reflection code here
+
+            Type classType = typeof(HarvestingFields);
+            FieldInfo[] getFields = classType.GetFields(BindingFlags.Static | BindingFlags.Instance
+                                                                | BindingFlags.NonPublic | BindingFlags.Public);
+
+            string input;
+            while ((input = Console.ReadLine()) != "HARVEST")
+            {
+                if (input == "private")
+                {
+                    getFields.Where(f => f.IsPrivate).ToList()
+                             .ForEach(f => Console.WriteLine($"{GetFieldModifier(f)} {f.FieldType.Name} {f.Name}"));
+                }
+                else if (input == "protected")
+                {
+                    getFields.Where(f => f.IsFamily).ToList()
+                             .ForEach(f => Console.WriteLine($"{GetFieldModifier(f)} {f.FieldType.Name} {f.Name}"));
+                }
+                else if (input == "public")
+                {
+                    getFields.Where(f => f.IsPublic).ToList()
+                             .ForEach(f => Console.WriteLine($"{GetFieldModifier(f)} {f.FieldType.Name} {f.Name}"));
+                }
+                else if (input == "all")
+                {
+                    getFields.ToList()
+                             .ForEach(f => Console.WriteLine($"{GetFieldModifier(f)} {f.FieldType.Name} {f.Name}"));
+                }
+            }
+        }
+
+        private static string GetFieldModifier(FieldInfo f)
+        {
+            if (f.IsFamily)
+            {
+                return "protected";
+            }
+            else if (f.IsPublic)
+            {
+                return "public";
+            }
+            else if (f.IsPrivate)
+            {
+                return "private";
+            }
+            else
+            {
+                throw new ArgumentException();
+            }
+        }
+    }
+}
---
+++ 04.2.Reflection_and_Attributes_Exercises/02.Black_Box_Integer/BlackBoxIntegerTests.cs
@@ -0,0 +1,33 @@
+﻿namespace P02_BlackBoxInteger
+{
+    using System;
+    using System.Linq;
+    using System.Reflection;
+
+    public class BlackBoxIntegerTests
+    {
+        public static void Main()
+        {
+            //TODO put your reflection code here
+
+            Type classType = typeof(BlackBoxInteger);
+            BlackBoxInteger activator = (BlackBoxInteger)Activator.CreateInstance(classType, true);
+
+            string cmd;
+            while ((cmd = Console.ReadLine()) != "END")
+            {
+                string command = cmd.Split('_').ToArray().Take(1).First();
+                int number = cmd.Split('_').ToArray().Skip(1).Take(1).Select(int.Parse).First();
+
+                MethodInfo method = classType.GetMethods(BindingFlags.Instance | BindingFlags.NonPublic)
+                                             .First(m => m.Name == command);
+                method.Invoke(activator, new object[] {number});
+
+                FieldInfo field = classType.GetFields(BindingFlags.Instance | BindingFlags.NonPublic)
+                                           .Where(f => f.Name == "innerValue").First();
+                Console.WriteLine($"{field.GetValue(activator)}");
+
+            }
+        }
+    }
+}
